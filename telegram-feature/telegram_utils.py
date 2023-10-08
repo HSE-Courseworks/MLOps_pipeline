a5dd5ad65eb0471701855ec2a280f223718f992b
sh1ro <artemrotfront@gmail.com>
2023-10-08 20:23:10 +0300

    update telegram_utils.py

--- telegram-feature/telegram_utils.py
+++ telegram-feature/telegram_utils.py
@@ -12,18 +12,45 @@ class TelegramClient:
     def get_n_last_posts(self, chat_id, n):
         with self.app:
             messages = list(self.app.get_chat_history(chat_id, limit=n))
-            for message in reversed(messages):
-                print(f"Post text: {message.text}\nViews: {message.views}")
+            data = {}
+            for i, message in enumerate(reversed(messages)):
+                post_text = message.text if message.text is not None else message.caption
+                data[i] = {
+                    'Post text': post_text, 
+                    'Views': message.views, 
+                    'Reactions': [],
+                    'Time': message.date
+                }
+                if message.photo is not None:
+                    data[i]['Media'] = message.photo.file_id
+                elif message.video is not None:
+                    data[i]['Media'] = message.video.file_id
+                else:
+                    data[i]['Media'] = None
                 if message.reactions is not None:
                     for reaction in message.reactions.reactions:
-                        print(f"Reaction: {reaction.emoji}\ncount: {reaction.count}\n")
+                        data[i]['Reactions'].append({'emoji': reaction.emoji, 'count': reaction.count})
+            return data
 
-api_id = int(input("Enter your API id: "))
-api_hash = input("Enter your API hash: ")
+if __name__ == "__main__":
+    api_id = int(input("Enter your API id: "))
+    api_hash = input("Enter your API hash: ")
 
-client = TelegramClient(api_id, api_hash)
-client.connect_to_channel()
+    client = TelegramClient(api_id, api_hash)
+    client.connect_to_channel()
 
-chat_id = input("Enter chat id: ")
-n = int(input("Enter the number of posts: "))
-posts = client.get_n_last_posts(chat_id, n)
+    chat_id = input("Enter chat id: ")
+    n = int(input("Enter the number of posts: "))
+
+    posts = client.get_n_last_posts(chat_id, n)
+
+    for i, post in posts.items():
+        print(f"Post {i+1}:")
+        print(f"Text: {post['Post text']}")
+        print(f"Views: {post['Views']}")
+        print(f"Time: {post['Time']}")
+        print(f"Media: {post['Media']}")
+        print("Reactions:")
+        for reaction in post['Reactions']:
+            print(f"Emoji: {reaction['emoji']}, Count: {reaction['count']}")
+        print("\n")
