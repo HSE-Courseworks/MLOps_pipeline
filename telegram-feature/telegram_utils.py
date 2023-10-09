f8e4f34b797e65eb82dbd0042877beef7e424e6d
sh1ro <artemrotfront@gmail.com>
2023-10-09 18:28:35 +0300

    fix getting media

--- telegram-feature/telegram_utils.py
+++ telegram-feature/telegram_utils.py
@@ -13,23 +13,37 @@ class TelegramClient:
         with self.app:
             messages = list(self.app.get_chat_history(chat_id, limit=n))
             data = {}
-            for i, message in enumerate(reversed(messages)):
-                post_text = message.text if message.text is not None else message.caption
-                data[i] = {
-                    'Post text': post_text, 
-                    'Views': message.views, 
-                    'Reactions': [],
-                    'Time': message.date
-                }
-                if message.photo is not None:
-                    data[i]['Media'] = message.photo.file_id
-                elif message.video is not None:
-                    data[i]['Media'] = message.video.file_id
+            i = -1
+            post_date = None
+            for message in reversed(messages):
+                if message.date != post_date:
+                    i += 1
+                    post_text = message.text if message.text is not None else message.caption
+                    data[i] = {
+                        'Post text': post_text, 
+                        'Views': message.views, 
+                        'Reactions': [],
+                        'Time': message.date,
+                        'Media': []
+                    }
+
+                    if message.photo is not None:
+                        data[i]['Media'].append({'type':'photo', 'id': message.photo.file_id})
+                    elif message.video is not None:
+                        data[i]['Media'].append({'type':'video', 'id': message.video.file_id})
+                    else:
+                        data[i]['Media'] = None
+                    if message.reactions is not None:
+                        for reaction in message.reactions.reactions:
+                            data[i]['Reactions'].append({'emoji': reaction.emoji, 'count': reaction.count})
+                    
+                    post_date = message.date
                 else:
-                    data[i]['Media'] = None
-                if message.reactions is not None:
-                    for reaction in message.reactions.reactions:
-                        data[i]['Reactions'].append({'emoji': reaction.emoji, 'count': reaction.count})
+                    if message.photo is not None:
+                        data[i]['Media'].append({'type':'photo', 'id': message.photo.file_id})
+                    elif message.video is not None:
+                        data[i]['Media'].append({'type':'video', 'id': message.video.file_id})
+            
             return data
 
 if __name__ == "__main__":
@@ -49,7 +63,9 @@ if __name__ == "__main__":
         print(f"Text: {post['Post text']}")
         print(f"Views: {post['Views']}")
         print(f"Time: {post['Time']}")
-        print(f"Media: {post['Media']}")
+        print(f"Media:")
+        for media in post['Media']:
+            print(f"Type: {media['type']}, ID: {media['id']}")
         print("Reactions:")
         for reaction in post['Reactions']:
             print(f"Emoji: {reaction['emoji']}, Count: {reaction['count']}")
