61f9aa7cbec34a69c901c3b3a8f96e989bf88926
sh1ro <artemrotfront@gmail.com>
2023-11-05 21:17:28 +0300

    deleting stopwords

--- telegram-feature/data_processing.py
+++ telegram-feature/data_processing.py
@@ -2,6 +2,11 @@ import sqlite3
 import nltk
 import re
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
+russian_stopwords = stopwords.words("russian")
 
 class DataProcessor:
     def __init__(self, db_path):
@@ -34,18 +39,26 @@ class DataProcessor:
             lower_case_data.append(lower_case_row)
         return lower_case_data
 
+    def remove_stopwords(self, data):
+        data_without_stopwords = []
+        for row in data:
+            row_without_stopwords = [[word for word in item if word not in russian_stopwords] for item in row]
+            data_without_stopwords.append(row_without_stopwords)
+        return data_without_stopwords
+
     def process_data(self, table_name):
         data = self.get_data(table_name)
         cleaned_data = self.remove_punctuation(data)
         tokenized_data = self.tokenize_data(cleaned_data)
         lower_case_data = self.to_lower_case(tokenized_data)
-        return lower_case_data
+        data_without_stopwords = self.remove_stopwords(lower_case_data)
+        return data_without_stopwords
 
     def close_connection(self):
         self.conn.close()
 
 if __name__ == '__main__':
     processor = DataProcessor('telegram_data.db')
-    lower_case_data = processor.process_data('posts')
-    print(lower_case_data)
+    data_without_stopwords = processor.process_data('posts')
+    print(data_without_stopwords)
     processor.close_connection()
