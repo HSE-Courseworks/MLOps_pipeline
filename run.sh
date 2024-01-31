c272c2b46df9b8dded2b6351ee3f8814d2342382
Sheykh Ruslan <ruslanqas@yandex.ru>
2024-01-31 18:30:32 +0300

    docker mlflow

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 docker-compose -f page_documentation/docker-compose.yaml up -d
 docker-compose -f FastAPI-app/docker-compose.yaml up -d
 echo -e "AIRFLOW_UID=$(id -u)" > .env
-docker-compose up  -d airflow-init
-docker-compose up -d 
+docker-compose up -d airflow-init
+docker-compose up -d
 sudo bash ./minio/minio.sh
