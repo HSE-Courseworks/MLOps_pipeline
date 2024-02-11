e00b7559ef56ec92b6ca9d19be97db96dfbbf93b
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-02-11 18:54:15 +0300

    adding a time stamp to logging

--- mlflow_experiments/mlflow_logging.py
+++ mlflow_experiments/mlflow_logging.py
@@ -1,9 +1,11 @@
 import mlflow
 import mlflow.sklearn
+from datetime import datetime
 from sklearn.metrics import mean_squared_error
 
 def log_model_and_metrics(model, model_name, X_train, y_train, X_test, y_test):
-    with mlflow.start_run(run_name=f"{model_name} Experiment") as run:
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    with mlflow.start_run(run_name=f"{model_name}_{timestamp}") as run:
         model.fit(X_train, y_train)
         predictions = model.predict(X_test)
         mse = mean_squared_error(y_test, predictions)
