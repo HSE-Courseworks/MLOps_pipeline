fd2691bc884249c78f783260b00950c81c47404d
Sheykh Ruslan <ruslanqas@yandex.ru>
2024-02-17 14:30:23 +0300

    combining minio and mlflow docker-compose

--- stop.sh
+++ stop.sh
@@ -2,8 +2,7 @@ docker compose \
     -f docker-compose.page_doc.yaml \
     -f docker-compose.FastAPI.yaml \
     -f docker-compose.airflow.yaml \
-    -f docker-compose.minio.yaml \
-    -f docker-compose.mlflow.yaml \
+    -f docker-compose.mlflow_minio.yaml \
     --env-file ./env/.env.airflow \
     --env-file ./env/.env.mlflow \
     --env-file ./env/.env.minio down
