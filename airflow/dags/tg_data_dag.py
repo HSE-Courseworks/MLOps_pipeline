1e825ecd497542fc5efbd3926290db681db389f5
Sheykh Ruslan <ruslanqas@yandex.ru>
2024-02-27 04:17:08 +0300

    maybe it's better this way

--- airflow/dags/tg_data_dag.py
+++ airflow/dags/tg_data_dag.py
@@ -9,11 +9,6 @@ args = {
     'provide_context': True
 }
 
-def retrieve_posts_channels(**kwargs):
-    channels = read_tg_channels()
-    for i, channel_id in enumerate(channels):
-        retrieve_posts(channel_id, 150)
-
 def retrieve_posts(channel_id, n, **kwargs):
     api_id, api_hash = read_tg_info()
     session_string = "AgFfAc4ALjzMYIRz_9iLm_ptYPK5cFGQARRXPakqBwVYTf6HWie7qDJ0WD2vXLCy99QJf63OnWllsvcQreSZF5TEKY1i0tGLUM2uU8fkIYVhdrxjyKU_6F20Eh-yRiZp6nCTsPCK8GQfsOy3QqNeI0FsgPGdbcL77kZ4mPRM3Pfh2JT8NPz0CvwbLbHjRejqts8UdskIbqPqPJ-kXpnfeCBDNA7l8OaSKx11mry7VPjCXoS6iMKZt4tQAlApvN7qgSps58V-YTJe2lhtn0cIKpp_cuBktINFiFEKmF3ztOtnpTXCyWCRCdXDA9y3eQQPLsVMG657OD9KXhojzdEwv1fAD6DHrwAAAAFs60KSAA"
@@ -21,5 +16,10 @@ def retrieve_posts(channel_id, n, **kwargs):
     client.get_n_last_posts(channel_id, n)
 
 with DAG('tg_data_dag', description='Retrieve last n posts from Telegram channels', schedule_interval='*/2 * * * *',  catchup=False, default_args=args) as dag:
-    t1 = PythonOperator(task_id='retrieve_posts_channel', python_callable=retrieve_posts_channels)
-    t1
+     channels = read_tg_channels()
+     for i, channel_id in enumerate(channels):
+         task = PythonOperator(
+             task_id=f'retrieve_posts_channel_{i}',
+             python_callable=retrieve_posts,
+             op_args=[channel_id,  150]
+         )
