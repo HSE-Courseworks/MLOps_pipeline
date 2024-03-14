c194c58e150e3a91625227a095659ea8eaef306b
Sheykh Ruslan <ruslanqas@yandex.ru>
2024-03-15 02:19:32 +0300

    fixed a bug with volumes

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+mkdir -p ./data/pgadmin_db
 docker compose \
     -f docker-compose.html_page.yaml \
     -f docker-compose.FastAPI.yaml \
@@ -5,4 +6,4 @@ docker compose \
     -f docker-compose.mlflow_minio.yaml \
     --env-file ./env/.env.airflow \
     --env-file ./env/.env.mlflow \
-    --env-file ./env/.env.minio up --build
+    --env-file ./env/.env.minio up
