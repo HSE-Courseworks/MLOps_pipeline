c2ca6fbe16ac3e24aeb9e9768118b0287a236927
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-03-23 12:30:01 +0300

    1

--- telegram_feature/telegram_bot.py
+++ telegram_feature/telegram_bot.py
@@ -16,23 +16,30 @@ def extract_section_content(section_id):
     else:
         return "Section not found."
 
+
 section_to_button = {
     "mainSection": "🏠 Main",
     "docsSection": "📚 Documentation",
     "airflowSection": "🛠️ Airflow",
-    "mlflowSection": "🤖 MLflow"
+    "mlflowSection": "🤖 MLflow",
 }
 
 button_to_section = {v: k for k, v in section_to_button.items()}
 
 telegram_bot = telebot.TeleBot("6662860222:AAF9dv2lK6IrowCYYA06EC05Zh3k_0xx1Cs")
 
+
 @telegram_bot.message_handler(commands=["start"])
 def send_welcome_message(message):
     custom_keyboard = types.ReplyKeyboardMarkup(row_width=2)
     for button_text in button_to_section.keys():
         custom_keyboard.add(types.KeyboardButton(button_text))
-    telegram_bot.send_message(message.chat.id, "Привет! Я бот. Содержимое какой страницы желаете получить?", reply_markup=custom_keyboard)
+    telegram_bot.send_message(
+        message.chat.id,
+        "Привет! Я бот. Содержимое какой страницы желаете получить?",
+        reply_markup=custom_keyboard,
+    )
+
 
 @telegram_bot.message_handler(func=lambda message: True)
 def handle_button_click(message):
@@ -40,6 +47,7 @@ def handle_button_click(message):
         section_id = button_to_section[message.text]
         content = extract_section_content(section_id)
         telegram_bot.send_message(message.chat.id, content)
+
 
 if __name__ == "__main__":
     telegram_bot.polling()
