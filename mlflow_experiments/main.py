74268ddaf580e0e302b5ed3161b74d1a39cb6d2f
Ruslan Sheykh <145142248+Sherukas@users.noreply.github.com>
2024-03-23 12:06:53 +0300

    Volumes (#19)
    * fixed a bug with volumes
    
    * deleted the excess
    
    * the download bug fix
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * lint
    
    * fixed deletion
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker
    
    * ci docker

--- mlflow_experiments/main.py
+++ mlflow_experiments/main.py
@@ -2,10 +2,15 @@ import mlflow
 import numpy as np
 from mlflow_experiments.data_loader import load_iris_dataset
 from mlflow_experiments.models import NaiveCustomModel
-from mlflow_experiments.mlflow_logging import log_model_and_metrics, load_and_print_model_details, predict_model
+from mlflow_experiments.mlflow_logging import (
+    log_model_and_metrics,
+    load_and_print_model_details,
+    predict_model,
+)
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+
 
 def train_and_predict_models(model_func, name, X_train, y_train, X_test, y_test):
     model = model_func()
@@ -14,26 +19,35 @@ def train_and_predict_models(model_func, name, X_train, y_train, X_test, y_test)
     M1 = round(mean_squared_error(y_test, P1), 4)
     return name, run_id, M1, P1
 
+
 def load_and_check_model(run_id, name, X_test, y_test, P1, M1, experiment_name):
     model_2 = load_and_print_model_details(run_id, name, experiment_name)
     P2 = model_2.predict(X_test)
     M2 = round(mean_squared_error(y_test, P2), 4)
     predictions_equal = np.allclose(P1, P2)
     metrics_close = abs(M1 - M2) <= 0.001
-    print(f"Model: {name}, Predictions Equal: {predictions_equal}, Metrics Close: {metrics_close}")
+    print(
+        f"Model: {name}, Predictions Equal: {predictions_equal}, Metrics Close: {metrics_close}"
+    )
+
 
 if __name__ == "__main__":
     mlflow.set_tracking_uri("http://127.0.0.1:5000")
     mlflow.set_experiment("CI/CD tests")
-    
+
     X_train, X_test, y_train, y_test = load_iris_dataset()
-    
+
     model_functions = [
-        (lambda: RandomForestRegressor(n_estimators=100, random_state=42), "RandomForest"),
+        (
+            lambda: RandomForestRegressor(n_estimators=100, random_state=42),
+            "RandomForest",
+        ),
         (lambda: NaiveCustomModel(), "NaiveCustomModel"),
-        (lambda: LinearRegression(), "SimpleAIModel")
+        (lambda: LinearRegression(), "SimpleAIModel"),
     ]
-    
+
     for model_func, name in model_functions:
-        name, run_id, M1, P1 = train_and_predict_models(model_func, name, X_train, y_train, X_test, y_test)
+        name, run_id, M1, P1 = train_and_predict_models(
+            model_func, name, X_train, y_train, X_test, y_test
+        )
         load_and_check_model(run_id, name, X_test, y_test, P1, M1)
--- mlflow_experiments/mlflow_logging.py
+++ mlflow_experiments/mlflow_logging.py
@@ -4,31 +4,34 @@ from datetime import datetime
 from mlflow_experiments.models import NaiveCustomModel
 from sklearn.metrics import mean_squared_error
 
+
 def predict_model(model, X_test):
     if isinstance(model, NaiveCustomModel):
         return model.predict(model, X_test)
     else:
         return model.predict(X_test)
 
+
 def log_model_and_metrics(model, model_name, X_train, y_train, X_test, y_test):
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    model_type = 'naive' if isinstance(model, NaiveCustomModel) else 'sklearn'
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    model_type = "naive" if isinstance(model, NaiveCustomModel) else "sklearn"
     with mlflow.start_run(run_name=f"{model_name}_{timestamp}") as run:
         model.fit(X_train, y_train)
         predictions = predict_model(model, X_test)
         mse = mean_squared_error(y_test, predictions)
         mlflow.log_metric("mse", mse)
-        if model_type == 'sklearn':
+        if model_type == "sklearn":
             mlflow.sklearn.log_model(model, f"{model_name}-model")
         else:
             mlflow.pyfunc.log_model(model_name, python_model=model)
-        return run.info.run_id 
+        return run.info.run_id
+
 
 def load_and_print_model_details(run_id, name, experiment_name):
     print(experiment_name)
     artifact_path = f"s3://mlflow/{mlflow.get_experiment_by_name(experiment_name).experiment_id}/{run_id}/artifacts/{name}"
-    model_type = 'naive' if name == "NaiveCustomModel" else 'sklearn'
-    if model_type == 'sklearn':
+    model_type = "naive" if name == "NaiveCustomModel" else "sklearn"
+    if model_type == "sklearn":
         return mlflow.sklearn.load_model(artifact_path + "-model")
     else:
         return mlflow.pyfunc.load_model(artifact_path)
