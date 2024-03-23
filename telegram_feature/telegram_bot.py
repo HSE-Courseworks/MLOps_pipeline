a3a0e4b5354e474db6ac4a39f19f24486f6288bf
ArtemOvsyannikov <artemrotfront@gmail.com>
2024-03-23 12:25:01 +0300

    1

--- telegram_feature/telegram_bot.py
+++ telegram_feature/telegram_bot.py
@@ -5,29 +5,29 @@ import requests
 
 
 def extract_section_content(section_id):
-    url = 'http://localhost:8888/'
+    url = "http://localhost:8888/"
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, "html.parser")
     section = soup.find(id=section_id)
     if section:
-        paragraphs = section.find_all('p')
-        formatted_text = '\n'.join([p.get_text(strip=True) for p in paragraphs])
+        paragraphs = section.find_all("p")
+        formatted_text = "\n".join([p.get_text(strip=True) for p in paragraphs])
         return formatted_text
     else:
         return "Section not found."
 
 section_to_button = {
-    'mainSection': '🏠 Main',
-    'docsSection': '📚 Documentation',
-    'airflowSection': '🛠️ Airflow',
-    'mlflowSection': '🤖 MLflow'
+    "mainSection": "🏠 Main",
+    "docsSection": "📚 Documentation",
+    "airflowSection": "🛠️ Airflow",
+    "mlflowSection": "🤖 MLflow"
 }
 
 button_to_section = {v: k for k, v in section_to_button.items()}
 
-telegram_bot = telebot.TeleBot('6662860222:AAF9dv2lK6IrowCYYA06EC05Zh3k_0xx1Cs')
+telegram_bot = telebot.TeleBot("6662860222:AAF9dv2lK6IrowCYYA06EC05Zh3k_0xx1Cs")
 
-@telegram_bot.message_handler(commands=['start'])
+@telegram_bot.message_handler(commands=["start"])
 def send_welcome_message(message):
     custom_keyboard = types.ReplyKeyboardMarkup(row_width=2)
     for button_text in button_to_section.keys():
@@ -41,5 +41,5 @@ def handle_button_click(message):
         content = extract_section_content(section_id)
         telegram_bot.send_message(message.chat.id, content)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     telegram_bot.polling()
