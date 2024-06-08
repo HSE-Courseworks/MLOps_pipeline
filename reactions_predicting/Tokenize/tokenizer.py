f104758f44d36736438c51fc3de2de0174b03f16
aeksin <aeksin06.05.2004@gmail.com>
2024-06-08 13:36:45 +0300

    post reactions prediction

---
+++ reactions_predicting/Tokenize/tokenizer.py
@@ -0,0 +1,42 @@
+from pymorphy2 import MorphAnalyzer
+import emoji
+import re
+class tokenize:
+    def __init__(self, text = ""):
+        self.text = text
+        self.morph = MorphAnalyzer()
+
+    def set_text(self,text):
+        self.text = text
+
+    def predict(self):
+        def remove_emoji(text):
+            return emoji.replace_emoji(text)
+
+        def remove_punctuation(word: str):
+            return re.sub(r'[.,:;]', '', word)
+
+        def encode_numbers(word: str):
+            if word.isnumeric():
+                return "I" + str(len(word))
+            elif word[1:].isnumeric():  # обработка чисел вида $*
+                return word[0] + "I" + str(len(word) - 1)
+            else:  # обработка чисел вида *$
+                return word[-1] + "I" + str(len(word) - 1)
+        self.text = remove_emoji(self.text)
+        words = self.text.split()  # разбиваем текст на слова
+        res = []
+        for word in words:
+            if word.isnumeric() or word[1:].isnumeric() or word[:-1].isnumeric():
+                word = encode_numbers(word)
+                res.append(word)
+            else:
+                word = remove_punctuation(word)
+                p = self.morph.parse(word)[0]
+                res.append(p.normal_form)
+
+        return res
+
+    def predict_with_set(self, text):
+        self.set_text(text)
+        return self.predict()
---
+++ reactions_predicting/main.py
@@ -0,0 +1,24 @@
+from predicter import reactions_predicter
+import sqlite3
+import pandas as pd
+def get_reactions(text: str):
+    predicter = reactions_predicter()
+
+def read_posts_from_db(database_path, table_name):
+    db_con = sqlite3.connect(f"{database_path}")
+    cur = db_con.cursor()
+    cur.execute(f"""
+        SELECT p.post_id, p.post_text, GROUP_CONCAT(emoji || count) AS reactions_dictionary
+    FROM {table_name} p
+    INNER JOIN reactions r ON p.post_id = r.post_id
+    GROUP BY p.post_id, post_text;
+    """)
+    data = pd.DataFrame(cur.fetchall(), columns=['post_id', 'post_text', 'reactions'])
+    data = data.iloc[1:, :]
+    data = data.set_index('post_id')
+    return data
+def main():
+    posts_with_reactions = read_posts_from_db("db/database.db", 'posts')
+    rp = reactions_predicter(posts_with_reactions, 'reactions')
+    print(rp.predict(text="кормен - лучшая книга по алгоритмам"))
+main()
